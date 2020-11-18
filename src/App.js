5c5662e9dc183babbb2ec0820e54c95aef42f370
Muhammed <nsaryyev9797@gmail.com>
2020-11-19 00:35:02 +0200

    Google Fonts and Bootstrap installed and prepared styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-6">Column number 1</div>
+          <div className="col-6">
+            <span>
+              <i className="fas fa-home"></i>
+            </span>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
+
+
