44815cab922f02e31a53ce5b35168ac7ca29e46d
Muhammed <nsaryyev9797@gmail.com>
2020-11-19 02:58:24 +0200

    Developed Navbar Component

--- src/App.js
+++ src/App.js
@@ -2,25 +2,29 @@ import './App.css';
 import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
+import { Route, Switch } from 'react-router-dom';
 
 export class App extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-6">Column number 1</div>
-          <div className="col-6">
-            <span>
-              <i className="fas fa-home"></i>
-            </span>
-          </div>
-        </div>
-      </div>
+      <React.Fragment>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' render={() => <ProductList />} />
+          <Route path='/details' render={() => <Details />} />
+          <Route path='/cart' render={() => <Cart />} />
+          <Route path='*' render={() => <Default />} />
+        </Switch>
+      </React.Fragment>
     )
   }
 }
 
+
+
 export default App
-
-
-
