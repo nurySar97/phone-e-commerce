017d5a960099bceb17304288df8c73e75931dd45
Muhammed <nsaryyev9797@gmail.com>
2020-11-19 15:05:46 +0200

    created Title component

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,12 +1,26 @@
 import React, { Component } from 'react'
 import Product from './Product'
+import Title from './Title'
 
 export class ProductList extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            products: []
+        }
+    }
     render() {
         return (
-            <div>
-               <Product />
-            </div>
+            <>
+                <div className="py-5">
+                    <div className="container">
+                        <Title name='our' title='products' />
+                        <div className="row">
+
+                        </div>
+                    </div>
+                </div>
+            </>
         )
     }
 }
