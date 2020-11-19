edfccb2998cec7fdbc9425c3e7607e7e8857d184
Muhammed <nsaryyev9797@gmail.com>
2020-11-19 14:47:37 +0200

    Created Button component and install styled components

---
+++ src/components/Button.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const ButtonContainer = styled.button`
+    text-transform: capitalize;
+    font-size: 1.4rem;
+    background: transparent;
+    border: 0.05rem solid var(--lightBlue);
+    border-color: var(--lightBlue);
+    color: var(--lightBlue);
+    border-radius: 0.5rem;
+    padding: 0.2rem 0.5rem;
+    cursor: pointer;
+    margin: 0.2rem 0.5rem 0.2rem 0;
+    transition: all 0.5s ease-in-out;
+    &:hover {
+        background: var(--lightBlue);
+        color: var(--mainBlue);
+    }
+    &:focus {
+        outline: none;
+    }
+`
+const Button = props => {
+    return (
+        <ButtonContainer>
+            {props.children}
+        </ButtonContainer>
+    )
+}
+
+export default Button
