401cd0ed3de6f580192162e3d3e4e0e4086e2904
Muhammed <nsaryyev9797@gmail.com>
2020-11-20 22:20:00 +0200

    Product Component added style

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -12,7 +12,7 @@ export class Product extends Component {
         return (
             <>
                 <ProductWrapper className='col-9 mx-auto col-md-6 col-lg-3'>
-                    <div className="card">
+                    <div className="card m-2">
                         <div className="img-container p-5" onClick={() => console.log("You Click To Me!")}>
                             <Link to='/details'>
                                 <img className='card-img-top' src={img} alt='product' />
@@ -50,7 +50,54 @@ export class Product extends Component {
 
 
 const ProductWrapper = styled.div`
-    
+    .card {
+        border-color: transparent;
+        transition: all 1s linear;
+    }
+    .card-footer {
+        background: transparent;
+        border-top: transparent;
+        transition: all 1s linear;
+    }
+    &:hover {
+        .card{
+            border: 0.04rem solid rgba(0, 0, 0, 0.2);
+            box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.2)
+        }
+    }
+    .card-footer{
+        background: rgba(247, 247, 247);
+    }
+    .img-container{
+        position: relative;
+        overflow: hidden;
+    }
+    .img-container .card-img-top {
+        transition: transform .2s linear;
+    }
+    .img-container:hover .card-img-top {
+        transform: scale(1.09);
+    }
+    .cart-btn {
+        position: absolute;
+        bottom: 0;
+        right: 0;
+        padding: .2rem .4rem;
+        background: var(--lightBlue);
+        border: none;
+        color: var(--mainWhite);
+        font-size: 1.4rem;
+        border-radius: .5rem 0 0 0;
+        transform: translate(100%, 100%);
+        transition: transform .2s linear;
+    }
+    .img-container:hover .cart-btn{
+        transform: translate(0, 0);
+    }
+    .cart-btn:hover{
+        color: var(--mainBlue);
+        cursor: pointer;
+    }
 `
 
 export default Product
